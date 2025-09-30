150111332469ccc90efc3d4cf64e7ac17df3d796
irfan-syed-mdt <irfan.syed@medtroniclabs.org>
2025-09-30 14:48:59 +0530

    Add springboot structure files

---
+++ src/test/java/org/mdl/smartcare/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mdl.smartcare.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
